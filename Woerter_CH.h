b493e2ff9a0e45c73ed2bbe87e509cd28eeb191a
Kim <kim.ferrari@vigilitech.ch>
2022-11-07 22:34:54 +0100

    Change CH to CH_AG words

--- Woerter_CH.h
+++ Woerter_CH.h
@@ -31,28 +31,28 @@
  * Definition der Woerter
  */
 #define CH_VOR          matrix[2] |= 0b0000000011100000
-#define CH_AB           matrix[3] |= 0b1100000000000000
+#define CH_AB           matrix[3] |= 0b0011000000000000
 #define CH_ESISCH       matrix[0] |= 0b1101111000000000
 
-#define CH_FUEF         matrix[0] |= 0b0000000011100000
-#define CH_ZAEAE        matrix[1] |= 0b0000000011100000
-#define CH_VIERTU       matrix[1] |= 0b1111110000000000
-#define CH_ZWAENZG      matrix[2] |= 0b1111110000000000
-#define CH_HAUBI        matrix[3] |= 0b0001111100000000
+#define CH_FUEF         matrix[1] |= 0b0000000111100000
+#define CH_ZAEAE        matrix[0] |= 0b0000000011100000
+#define CH_VIERTU       matrix[1] |= 0b1111111000000000
+#define CH_ZWAENZG      matrix[2] |= 0b0111111000000000
+#define CH_HAUBI        matrix[3] |= 0b0000011111000000
 
-#define CH_H_EIS        matrix[4] |= 0b1110000000000000
-#define CH_H_ZWOEI      matrix[4] |= 0b0001111000000000
-#define CH_H_DRUE       matrix[4] |= 0b0000000011100000
+#define CH_H_EIS        matrix[6] |= 0b0011100000000000
+#define CH_H_ZWOEI      matrix[6] |= 0b0000011110000000
+#define CH_H_DRUE       matrix[9] |= 0b0000000011100000
 #define CH_H_VIER       matrix[5] |= 0b1111100000000000
-#define CH_H_FUEFI      matrix[5] |= 0b0000011110000000
-#define CH_H_SAECHSI    matrix[6] |= 0b1111110000000000
-#define CH_H_SIEBNI     matrix[6] |= 0b0000001111100000
-#define CH_H_ACHTI      matrix[7] |= 0b1111100000000000
-#define CH_H_NUENI      matrix[7] |= 0b0000011110000000
-#define CH_H_ZAENI      matrix[8] |= 0b1111000000000000
-#define CH_H_EUFI       matrix[8] |= 0b0000000111100000
-#define CH_H_ZWOEUFI    matrix[9] |= 0b1111110000000000
+#define CH_H_FUEFI      matrix[5] |= 0b0000011111000000
+#define CH_H_SAECHSI    matrix[4] |= 0b1111110000000000
+#define CH_H_SIEBNI     matrix[4] |= 0b0000001111100000
+#define CH_H_ACHTI      matrix[8] |= 0b0111110000000000
+#define CH_H_NUENI      matrix[8] |= 0b0000001111000000
+#define CH_H_ZAENI      matrix[7] |= 0b1111100000000000
+#define CH_H_EUFI       matrix[7] |= 0b0000000111100000
+#define CH_H_ZWOEUFI    matrix[9] |= 0b0111111000000000
 
-#define CH_GSI        matrix[9] |= 0b0000000011100000
+#define CH_GSI          matrix[9] |= 0b0000000000000000
 
 #endif
